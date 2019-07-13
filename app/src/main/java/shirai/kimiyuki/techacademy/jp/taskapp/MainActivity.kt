617f88216d78c71422828bad5f52179cc9a54087
kimiyuki shirai <kimiyuki@gmail.com>
2019-07-13 12:44:06 +0900

    wip add Category class

--- app/src/main/java/shirai/kimiyuki/techacademy/jp/taskapp/MainActivity.kt
+++ app/src/main/java/shirai/kimiyuki/techacademy/jp/taskapp/MainActivity.kt
@@ -164,3 +164,15 @@ open class Task: RealmObject(), Serializable{
     var id:Int = 0
 
 }
+
+class Category {
+    var name: String = ""
+    var id: Int = 0
+    /*
+        上記のString型のcategoryを、クラスのCategoryへ変更してください
+        追加で、タスク作成画面から遷移する画面を1つ作成してください
+        その画面ではCategory（idとカテゴリ名を持つ）のクラスを作成できるようにしてください
+        タスク作成画面でTaskを作成するときにCategoryを選択できるようにしてください
+        一覧画面でCategoryを選択すると、Categoryに属しているタスクのみ表示されるようにしてください
+     */
+}
