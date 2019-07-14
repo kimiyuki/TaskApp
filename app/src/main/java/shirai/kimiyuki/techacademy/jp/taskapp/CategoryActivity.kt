d1e1d10fbf59f3665960bc9315272fa834cdcbb6
kimiyuki shirai <kimiyuki@gmail.com>
2019-07-15 08:52:13 +0900

    add CategoryActivity

---
+++ app/src/main/java/shirai/kimiyuki/techacademy/jp/taskapp/CategoryActivity.kt
@@ -0,0 +1,12 @@
+package shirai.kimiyuki.techacademy.jp.taskapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CategoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+    }
+}
