5692eae5acf576d4dade815c241c4343a843a826
kimiyuki shirai <kimiyuki@gmail.com>
2019-07-17 17:32:58 +0900

    wip: CategoryActivity functions

--- app/src/main/java/shirai/kimiyuki/techacademy/jp/taskapp/CategoryActivity.kt
+++ app/src/main/java/shirai/kimiyuki/techacademy/jp/taskapp/CategoryActivity.kt
@@ -2,6 +2,8 @@ package shirai.kimiyuki.techacademy.jp.taskapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import io.realm.Realm
 import io.realm.RealmChangeListener
 import kotlinx.android.synthetic.main.activity_category.*
@@ -11,12 +13,6 @@ class CategoryActivity : AppCompatActivity() {
 
     private lateinit var mRealm: Realm
     private lateinit var mCategoryAdapter: CategoryAdapter
-    private var categoryList = mutableListOf<Category>()
-    private val mRealmListener = object : RealmChangeListener<Realm> {
-        override fun onChange(t: Realm) {
-            reloadListViewCategory(null)
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,26 +20,26 @@ class CategoryActivity : AppCompatActivity() {
 
         //Realm
         mRealm = Realm.getDefaultInstance()
-        mRealm.addChangeListener(mRealmListener)
-
-        //listView
-        mCategoryAdapter = CategoryAdapter(this@CategoryActivity)
+        mRealm.addChangeListener{reloadListViewCategory(null)}
 
         //show data
         reloadListViewCategory(null)
-
     }
 
     private fun reloadListViewCategory(query: String?) {
+        //listView
         val categoryRealmResults = if (query != null) {
-            mRealm.where(Category::class.java)
-                .contains("name", query)
-                .findAll()
+            mRealm.where(Category::class.java).contains("name", query).findAll()
         } else {
             mRealm.where(Category::class.java).findAll()
         }
-        //TODO
-//        mCategoryAdapter = mRealm.copyFromRealm(categoryRealmResults)
-//        listViewCategory.adapter = mCategoryAdapter
+        mCategoryAdapter = CategoryAdapter(this@CategoryActivity,
+            categoryRealmResults.toMutableList()) {
+            Log.d("hello_category", it?.name)
+        }
+        Log.d("hello reload", categoryRealmResults.size.toString())
+        listViewCategory.adapter = mCategoryAdapter
+        listViewCategory.layoutManager = LinearLayoutManager(
+            this, LinearLayoutManager.VERTICAL, false)
     }
 }
--- app/src/main/java/shirai/kimiyuki/techacademy/jp/taskapp/CategoryAdapter.kt
+++ app/src/main/java/shirai/kimiyuki/techacademy/jp/taskapp/CategoryAdapter.kt
@@ -2,41 +2,36 @@ package shirai.kimiyuki.techacademy.jp.taskapp
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import io.realm.Realm
-import io.realm.RealmResults
 import kotlinx.android.synthetic.main.row_categories.view.*
 import shirai.kimiyuki.techacademy.jp.taskapp.Models.Category
 
-class CategoryAdapter(context: Context, val onItemClicked: (Category?) -> Unit):
+class CategoryAdapter(context: Context, val categories: MutableList<Category>,
+                      val onItemClicked: (Category?) -> Unit):
     RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
 
-    val mLayoutInflater: LayoutInflater = LayoutInflater.from(context)
-    private lateinit var mRealm : Realm
-    private lateinit var ret: RealmResults<Category>
+    private val mLayoutInflater: LayoutInflater = LayoutInflater.from(context)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder{
-        mRealm = Realm.getDefaultInstance()
-        ret = mRealm.where(Category::class.java).findAll()
-        mRealm.close()
-
         val view = mLayoutInflater.inflate(R.layout.row_categories, parent, false)
         val holder = CategoryViewHolder(view)
         view.setOnClickListener{
            val position = holder.adapterPosition
-           val category = ret[position]
+           val category = categories[position]
             onItemClicked(category)
         }
         return holder
     }
 
-    override fun getItemCount(): Int = ret.size
+    override fun getItemCount(): Int = categories.size
 
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-        holder.category_id.text = ret[position]?.id.toString()
-        holder.category_name.text = ret[position]?.name.toString()
+        Log.d("hello BindBiewHolder", categories[position]?.name.toString())
+        holder.category_id.text = categories[position]?.id.toString()
+        holder.category_name.text = categories[position]?.name.toString()
     }
 
     class CategoryViewHolder(view: View): RecyclerView.ViewHolder(view){
